8f4cd83fb77f3df9a42ca7f2ef6d847cf173ce99
unknown <coding812@gmail.com>
2023-12-18 01:39:25 -0500

    setting up routing

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+import { DrawerLayoutAndroid } from 'react-native';
 
-export default function App() {
+import Home from './Views/Home';
+import CapCalc from './Views/CapCalc';
+
+const Stack = createStackNavigator();
+
+export default function App() 
+{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Capacitor Calculator" component={CapCalc} />
+      </Stack.Navigator>
+
+
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: 
+//   {
+//     flex: 1,
+//     backgroundColor: 'yellow',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
