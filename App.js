ee13fef606e4001094777abe7202e1789c836efc
unknown <coding812@gmail.com>
2023-12-18 13:32:17 -0500

    organized views, got navigation working, added capCalc page

--- App.js
+++ App.js
@@ -1,35 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator} from '@react-navigation/stack';
-import { DrawerLayoutAndroid } from 'react-native';
 
-import Home from './Views/Home';
-import CapCalc from './Views/CapCalc';
-
-const Stack = createStackNavigator();
+import DrawerNavigator from './Shared/DrawerNavigator';
 
 export default function App() 
 {
   return (
     <NavigationContainer>
-      <StatusBar style="auto" />
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Capacitor Calculator" component={CapCalc} />
-      </Stack.Navigator>
-
-
+      {/* <StatusBar style="auto" /> */}
+      <DrawerNavigator />
     </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: 
-//   {
-//     flex: 1,
-//     backgroundColor: 'yellow',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- Views/CapCalc.js
+++ Views/CapCalc.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
-const CapCalc = () => {
+const CapCalc = () => 
+{
+    const [amps, setAmps] = useState(0);
+    const [voltage, setVoltage] = useState(0);
+    const [capacitance, setCapacitance] = useState(0);
+
+    function CalculateCapacitance() 
+    {
+        const calculatedCapacitance = (amps * 2652) / voltage;
+        setCapacitance((calculatedCapacitance).toFixed(0));
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Capacitor Calculator</Text>
+            <TextInput 
+                id='Amps'
+                placeholder='Amps on Start Winding'
+                style={{ height: 40, width: 230, textAlign: 'center', borderColor: 'gray', borderWidth: 1, borderRadius: 8, marginTop: 20 }}
+                onChangeText={text => setAmps(parseFloat(text))}
+                value={amps.toString()}
+                keyboardType='numeric' 
+            />
+            <TextInput 
+                id='Voltage'
+                placeholder='Voltage between Common and Run'
+                style={{ height: 40, width: 230, textAlign: 'center', borderColor: 'gray', borderWidth: 1, borderRadius: 8, marginTop: 10 }}
+                onChangeText={text => setVoltage(parseFloat(text))}
+                value={voltage.toString()}
+                keyboardType='numeric'
+            />
+
+            <Text style={{ marginTop: 10 }} onPress={CalculateCapacitance}>Calculate</Text>
+            <Text style={{ marginTop: 10 }}>Calculated Capacitance: {capacitance} MicroFarads</Text>
         </View>
     );
 };
@@ -16,7 +46,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     text: {
-        fontSize: 24,
+        fontSize: 26,
         fontWeight: 'bold',
     },
 });
